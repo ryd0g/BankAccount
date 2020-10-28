f2ab4f0103135f0284f362492c0d55e2f9035ca4
Ryan Lee <rjlguitar@gmail.com>
2020-10-27 17:36:44 -0700

    fixed functions

--- bankaccount.py
+++ bankaccount.py
@@ -6,24 +6,27 @@ class BankAccount:
         self.balance = balance
 
     def deposit(self, amount):
-        total = balance + amount
-        print(f"Amount Deposited: ${self.amount}")
+        total = self.balance + amount
+        print(f"Amount Deposited: ${total}")
 
     def withdraw(self, amount):
-        total = balance - amount
+        total = self.balance - amount
         if amount > balance:
             print(f"Insufficient Funds. You have been charged an overdraft fee of $10")
         else:
-            print(f"Amount Withdrawn: ${self.amount}")
+            print(f"Amount Withdrawn: ${total}")
 
     def get_balance(self):
         return self.balance
         print(f"Your balance is ${self.balance}")
     
     def add_interest(self):
-        interest = balance * 0.00083
-        print(f"Your monthly interest is ${self.interest}")
+        interest = self.balance * 0.00083
+        print(f"Your monthly interest is ${interest}")
 
     def print_receipt(self):
-        receipt = [full_name, account_number, routing_number, balance]
+        receipt = [self.full_name, self.account_number, self.routing_number, self.balance]
         print(receipt)
+
+customer_one = BankAccount('Ryan Lee', '29573942', '968473963', 1000)
+customer_one.print_receipt()
