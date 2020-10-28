19c14edfb333d68e2709ffdcc00bfa624569630a
Ryan Lee <rjlguitar@gmail.com>
2020-10-27 17:30:01 -0700

    added methods

--- bankaccount.py
+++ bankaccount.py
@@ -4,3 +4,26 @@ class BankAccount:
         self.account_number = account_number
         self.routing_number = routing_number
         self.balance = balance
+
+    def deposit(self, amount):
+        total = balance + amount
+        print(f"Amount Deposited: ${self.amount}")
+
+    def withdraw(self, amount):
+        total = balance - amount
+        if amount > balance:
+            print(f"Insufficient Funds. You have been charged an overdraft fee of $10")
+        else:
+            print(f"Amount Withdrawn: ${self.amount}")
+
+    def get_balance(self):
+        return self.balance
+        print(f"Your balance is ${self.balance}")
+    
+    def add_interest(self):
+        interest = balance * 0.00083
+        print(f"Your monthly interest is ${self.interest}")
+
+    def print_receipt(self):
+        receipt = [full_name, account_number, routing_number, balance]
+        print(receipt)
