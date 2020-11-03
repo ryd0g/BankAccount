1544338890f190c5b7ed7ff05c3c77001cbad75b
Ryan Lee <rjlguitar@gmail.com>
2020-11-02 17:17:32 -0800

    added line indent after statements

--- bankaccount.py
+++ bankaccount.py
@@ -8,7 +8,7 @@ class BankAccount:
     def deposit(self, amount):
         total = self.balance + amount
         self.balance = total
-        print(f'Amount Deposited: ${amount}. Your new balance is ${self.balance}')
+        print(f'Amount Deposited: ${amount}. Your new balance is ${self.balance}\n')
 
     def withdraw(self, amount):
         total = self.balance - amount
@@ -16,14 +16,14 @@ class BankAccount:
         if amount > self.balance:
             print(f"Insufficient Funds. You have been charged an overdraft fee of $10")
         else:
-            print(f'Amount Withdrawn: ${amount}. Your new balance is ${self.balance}')
+            print(f'Amount Withdrawn: ${amount}. Your new balance is ${self.balance}\n')
 
     def get_balance(self):
-        print(f"Your balance is ${self.balance}")
+        print(f"Your balance is ${self.balance}\n")
     
     def add_interest(self):
         interest = self.balance * 0.00083
-        print(f"Your monthly interest is ${interest}")
+        print(f"Your monthly interest is ${interest}\n")
 
     def print_receipt(self):
         receipt = [self.full_name, 'Account No.: ****' + str(self.account_number[-4:]), 
@@ -35,5 +35,6 @@ class BankAccount:
 customer_one = BankAccount('Ryan Lee', '29573942', '968473963', 1000)
 customer_one.deposit(100)
 customer_one.print_receipt()
+
 customer_two = BankAccount('Jeff Bezos', '06939531', '206927539', 203000000000)
 customer_three = BankAccount('Donald Trump', '95846212', '235723468', 2500000000)
